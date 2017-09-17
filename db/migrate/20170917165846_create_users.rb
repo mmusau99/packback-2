b3a27939a8ff33cb551f9268b2765a665f978adc
mmusau99 <mmusau99@gmail.com>
2017-09-17 12:59:49 -0500

    fixed questions panel size

--- db/migrate/20170917165846_create_users.rb
+++ db/migrate/20170917165846_create_users.rb
@@ -2,13 +2,10 @@ class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       t.string :email
-      t.string :string
       t.string :full_name
       t.string :phone_number
-      t.string :string
       t.string :course
-      t.string :course_size
-      t.string :integer
+      t.integer :course_size
 
       t.timestamps
     end
